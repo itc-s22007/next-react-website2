8752414d97750a1ea53f832c74f53a446ad81af4
s22007 <s22007>
2023-02-16 09:41:54 +0000

    chap8

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,12 @@
+import Meta from 'components/meta'
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Blog () {
   return (
-    <Hero
-      title='Blog'
-      subtitle='Recent Posts'
-    />
+    <Container>
+      <Meta pageTitle='ブログ' />
+      <Hero title='Blog' subtitle='Recent Posts' />
+    </Container>
   )
 }
